14341c1cc64e1dbc9f0921aec6250f3ada9dd9a0
José Miguel <developed4web@gmail.com>
2014-02-11 14:32:50 -0300

    ejemplo de creación de array y acceso al primer elemento.

--- php5.4/arrays/arrays01.php
+++ php5.4/arrays/arrays01.php
@@ -1,2 +1,10 @@
 <?php
 echo "Ejemplos de uso:";
+
+echo "Declarar array e inicializarlo";
+
+$frutas = array('pera', 'manzana', 'frutilla', 'durazno', 'sandia');
+
+echo "Acceder al primer elemento del array";
+echo $frutas[0];
+
